641c7fc9b7e8071551466a931d761f40695f4888
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-10 23:36:10 +0200

    Create chat_codes.py

---
+++ Exercise: Basic Syntax, Conditional Statements, and Loops/codes_exercise/chat_codes.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+for i in range(n):
+    number = int(input())
+
+    if number == 88:
+        print("Hello")
+    elif number == 86:
+        print("How are you?")
+    elif number != 88 and number != 86 and number < 88:
+        print("GREAT!")
+    elif number > 88:
+        print("Bye.")
