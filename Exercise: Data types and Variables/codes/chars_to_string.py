c9125413d43f242b5ec3d30159baf5c1f31f9fb5
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-17 22:03:23 +0200

    Delete Exercise: Data types and Variables/codes directory

--- Exercise: Data types and Variables/codes/chars_to_string.py
+++
@@ -1,7 +0,0 @@
-firs_str = input()
-second_str = input()
-third_str = input()
-
-def MakeStr():
-  print(f"{firs_str}{second_str}{third_str}")
-MakeStr()
--- Exercise: Data types and Variables/codes/triple_chars.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-
-for i in range(0, num):
-    for k in range(0, num):
-        for j in range(0, num):
-            print(f"{chr( 97 + i)}{chr( 97 + k)}{chr( 97 + j)}")
