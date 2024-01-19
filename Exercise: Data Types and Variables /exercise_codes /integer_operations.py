493976b76291da11dd131589342e1d829fa3e88a
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-19 22:10:32 +0200

    Create integer_operations.py

---
+++ Exercise: Data Types and Variables /exercise_codes /integer_operations.py
@@ -0,0 +1,10 @@
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+fourth_num = int(input())
+
+sum = first_num + second_num
+sum = int(sum / third_num)
+sum = int(sum * fourth_num)
+
+print(sum)
