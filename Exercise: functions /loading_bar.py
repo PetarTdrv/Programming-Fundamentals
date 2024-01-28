0e31f9121a054ba3e1e70b0c1737b5a5b4e2b43d
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-28 21:06:54 +0200

    Create loading_bar.py

---
+++ Exercise: functions /loading_bar.py
@@ -0,0 +1,31 @@
+procents = int(input())
+
+if procents == 10:
+    print("10% [%.........]")
+    print("Still loading...")
+elif procents == 20:
+    print("20% [%%........]")
+    print("Still loading...")
+elif procents == 30:
+    print("30% [%%%.......]")
+    print("Still loading...")
+elif procents == 40:
+    print("40% [%%%%......]")
+    print("Still loading...")
+elif procents == 50:
+    print("50% [%%%%%.....]")
+    print("Still loading...")
+elif procents == 60:
+    print("60% [%%%%%%....]")
+    print("Still loading...")
+elif procents == 70:
+    print("70% [%%%%%%%...]")
+    print("Still loading...")
+elif procents == 80:
+    print("80% [%%%%%%%%..]")
+    print("Still loading...")
+elif procents == 90:
+    print("90% [%%%%%%%%%.]")
+    print("Still loading...")
+elif procents == 100:
+    print("100% Complete!\n[%%%%%%%%%%]")
