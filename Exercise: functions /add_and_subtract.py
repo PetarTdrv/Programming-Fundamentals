1960ab556ae322463e13a592ef374aabe4e60a42
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-28 21:02:55 +0200

    Create add_and_subtract.py

---
+++ Exercise: functions /add_and_subtract.py
@@ -0,0 +1,10 @@
+def add_and_subtract(first, second, third):
+    result = first + second
+    result = result - third
+    return result
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(add_and_subtract(first_number, second_number, third_number))
